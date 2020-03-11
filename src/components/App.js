398e8922b96e1c3a581334953dfb5e93230d17ab
iamasianman <kevinle1991@gmail.com>
2020-03-10 22:27:52 -0700

    added components files

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './NavBar';
+import Home from './Home'
+import Projects from './Projects'
+import About from './About'
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Projects />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h1> my header </h1>
+  )
+}
+
+export default Header;
